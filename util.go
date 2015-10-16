aa5e1c8577a07d38316948c7d44c647040db9d32
Michael Tremel <itrekkie@gmail.com>
2015-10-15 19:19:36 -0700

    get current marker date

---
+++ util.go
@@ -0,0 +1,14 @@
+package main
+
+import "time"
+
+const marker = 24
+
+func currentMarkerDay() time.Time {
+	rightNow := time.Now()
+	thisMonth := rightNow.Month()
+	thisYear := rightNow.Year()
+
+	marker := time.Date(thisYear, thisMonth, marker, 0, 0, 0, 0, time.Local)
+	return marker.AddDate(0, -1, 0)
+}
